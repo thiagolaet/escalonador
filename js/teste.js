62b390364b09724ef07f9843360e15ab0422c973
Thiago Laet <thi4golaet@gmail.com>
2020-10-05 12:57:09 -0300

    Commit inicial

---
+++ js/teste.js
@@ -0,0 +1,8 @@
+function changeState() {
+  document.querySelector('.introducao').classList.toggle('invisivel');
+  document.querySelector('.simulador').classList.toggle('invisivel');
+}
+
+function initialize() {
+
+}
